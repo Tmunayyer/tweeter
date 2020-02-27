321002fb9a7d3b80e157facfa8da5e670b7e0334
Thomas Munayyer <tmunayyer@gmail.com>
2020-02-27 16:49:01 -0500

    package json and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   mode: process.env.MODE || 'production',
   entry: './src/index.jsx',
-
+  performance: { hints: false },
   module: {
     rules: [
       {
